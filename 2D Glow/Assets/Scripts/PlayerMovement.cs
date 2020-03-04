6582227f29f337c225d3d4ebb9505472f615dd6b
Asbjørn Thirslund <at@brackeys.com>
2020-03-04 14:44:12 +0100

    Initial commit

---
+++ 2D Glow/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	public float speed = 5f;
+
+	public Animator animator;
+	public Rigidbody2D rb;
+
+	Vector2 move;
+
+	SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		move.x = Input.GetAxisRaw("Horizontal");
+
+		animator.SetFloat("Speed", Mathf.Abs(move.x));
+
+		if (Input.GetButtonDown("Fire1"))
+		{
+			animator.SetTrigger("Attack");
+		}
+    }
+
+	private void FixedUpdate()
+	{
+		rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
+
+		if (move.x < 0f)
+		{
+			sr.flipX = true;
+		} else if (move.x > 0f)
+		{
+			sr.flipX = false;
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# 2D-Glow
+# 2D Glow
 Project files for our tutorial on how to create 2D Glow in Unity.
+
+
+Check out our [YouTube Channel](http://youtube.com/brackeys) for more tutorials.
